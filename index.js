ece9d90e31f915da5c106c1f13f380f212febe69
user <user@MacBook-Pro-de-user.local>
2025-10-24 20:11:11 -0300

    save

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "dist/flexy")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/flexy", "index.html"));
+  res.sendFile(path.join(__dirname, "dist/Flexy", "index.html"));
 });
 
 // Catch-all handler: send back index.html for any route (SPA support)
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/flexy", "index.html"));
+  res.sendFile(path.join(__dirname, "dist/Flexy", "index.html"));
 });
 
 app.listen(PORT, () => {
