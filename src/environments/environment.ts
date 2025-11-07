ed27dac992484f9b0579e3be4306721c69f2257d
nicolasalvarezjs <nicolas.alvarez.js@gmail.com>
2025-11-07 16:56:30 -0300

    save

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl:'http://localhost:3002/api',
+  apiBaseUrl: 'https://vendors-service-stage.up.railway.app/api',
+  //apiBaseUrl:'http://localhost:3002/api',
 };
