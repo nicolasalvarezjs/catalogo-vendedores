9a690e28e246b5d48b0665b1b7fbbb076245b7e4
nicolasalvarezjs <nicolas.alvarez.js@gmail.com>
2025-11-10 16:30:47 -0300

    save

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  staging: true,
+  apiBaseUrl: 'https://dezain-server-stage.up.railway.app/api',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://dezain-server-stage.up.railway.app/api',
-  //apiBaseUrl:'http://localhost:3002/api',
+  staging: false,
+  apiBaseUrl: 'http://localhost:3002/api',
 };
